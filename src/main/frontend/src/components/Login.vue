2922910f2a5f7538c8b001c55f08f11b71c6e248
guillermo <h.uhm292@gmail.com>
2023-02-03 14:14:15 +0100

    login_page init

---
+++ src/main/frontend/src/components/Login.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { ref } from 'vue';
+let user = ref('');
+let password = ref('');
+
+</script>
+<template>
+  
+  
+  <main>
+  
+     <h1> {{ user }}</h1>
+     <h1>{{ password }}</h1> 
+      
+    <v-text-field
+      label = "User:"
+      :rules="rules"
+      hide-details="auto" v-model="user" >
+    </v-text-field>
+
+    <v-text-field
+      label = "Password:"
+      type="password"
+      :rules="rules"
+      hide-details="auto" v-model="password">
+    </v-text-field>
+
+    <v-btn rounded>CONNECT</v-btn>
+    <div>
+      <a href="">Forgot your password?</a>
+
+    
+
+    </div>
+    
+      
+    
+    
+    
+    
+  </main>
+</template>
+
+
+<style lang="scss" scoped>
+@use "../sass/styles.scss" as *;
+main{
+  background-color: $background-box;
+}
+
+    
+  </style>
