e2e9eed68767a28118bf39595d46c34e60da000a
guillermo <h.uhm292@gmail.com>
2023-02-06 15:35:31 +0100

    div navegation

--- src/main/frontend/src/views/HomeView.vue
+++ src/main/frontend/src/views/HomeView.vue
@@ -1,11 +1,17 @@
 <script setup>
+import HeaderVue from "../components/Header.vue";
 import LoginVue from "../components/Login.vue";
+import NavVue from "../components/Nav.vue";
 </script>
 
 <template>
   <main> 
-    <LoginVue></LoginVue>
+    <!-- <LoginVue></LoginVue> -->
+    <!-- <HeaderVue></HeaderVue> -->
+    <NavVue></NavVue>
+
   </main>
+  
 </template>
 
 
